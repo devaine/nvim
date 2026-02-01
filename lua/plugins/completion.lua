57222dd2397b034a7a05ffe676a4311783c4d835
devaine <devaine@devdoes.work>
2026-01-31 20:52:26 -0600

    fix(keybinds): created much nicer keybinds (like vscode) completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -12,28 +12,51 @@ return {
 		opts = {
 			keymap = {
 				preset = "none",
-				['<Tab>'] = { 'select_next', 'fallback' },
-				['<S-Tab>'] = { 'select_prev', 'fallback' },
-				["<CR>"] = { 'select_and_accept', 'fallback' },
-				['<C-e>'] = { 'hide', 'fallback' }
+				["<Tab>"] = {
+					function(cmp)
+						local cursor_pos = vim.api.nvim_win_get_cursor(0)
+						local col = cursor_pos[2]
+						local current_line = vim.api.nvim_get_current_line()
+
+						if cmp.is_menu_visible() then
+							cmp.select_next()
+							return
+						end
+
+						if current_line:sub(col, col) == "." then
+							if not cmp.is_menu_visible() then
+								cmp.show()
+								return
+							end
+						end
+						return "\t"
+					end,
+				},
+				["<S-Tab>"] = { "select_prev", "fallback" },
+				["<CR>"] = { "select_and_accept", "fallback" },
+				["<C-e>"] = { "hide", "fallback" },
 			},
 
 			completion = {
+				trigger = {
+					show_on_x_blocked_trigger_characters = { "." },
+				},
 				documentation = {
 					auto_show = true,
-					treesitter_highlighting = true
+					treesitter_highlighting = true,
 				},
 				menu = { auto_show = true },
-				ghost_text = { enabled = true }
+				ghost_text = { enabled = true },
 			},
 
 			sources = {
-				default = { 'lsp', 'path', 'snippets', 'buffer' },
+				default = { "lsp", "path", "snippets", "buffer" },
+				min_keyword_length = 0,
 			},
 
 			signature = { enabled = true },
 
-			fuzzy = { implementation = "prefer_rust_with_warning" }
-		}
-	}
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+	},
 }
